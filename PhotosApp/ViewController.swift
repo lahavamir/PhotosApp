17ec0e0e5eefa401360f5c9e53b580f64aa6bb55
Amir Lahav <lahavamir27@gmail.com>
2019-04-07 18:10:27 +0300

    Initial Commit

---
+++ PhotosApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotosApp
+//
+//  Created by Amir lahav on 07/04/2019.
+//  Copyright © 2019 la-labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
